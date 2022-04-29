0aac3db66bc814f09216a5ddf2dacb9f5b7ea41b
rayanabdirahman <rayanmabdirahman@gmail.com>
2022-04-29 19:26:03 +0100

    add boilerplate redux config

---
+++ state/domain/enums.ts
@@ -0,0 +1,3 @@
+export type UIState = {
+  isLoading: { action: string }[];
+};
---
+++ state/middlewares/index.ts
@@ -0,0 +1,6 @@
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+const middlewares = [thunk, logger];
+
+export default middlewares;
