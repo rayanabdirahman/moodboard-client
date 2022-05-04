2ed35fcf720e9fe6c72015071af3bb45c409a216
rayanabdirahman <rayanmabdirahman@gmail.com>
2022-05-04 21:25:45 +0100

    add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import NotFound from '../components/features/pageUtil/NotFound';
+
+const NotFoundPage: NextPage = () => {
+  return <NotFound />;
+};
+
+export default NotFoundPage;
