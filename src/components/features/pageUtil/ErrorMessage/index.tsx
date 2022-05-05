bb1bca42a17d8cc71f6b1b73685436f731ec82f6
rayanabdirahman <rayanmabdirahman@gmail.com>
2022-05-05 23:33:51 +0100

    add full page error component

---
+++ src/components/features/pageUtil/ErrorMessage/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const FullPageError = () => {
+  return (
+    <div className="container mx-auto mt-20 text-center">
+      <p>Uh oh... There`s a problem. Try refreshing the app.</p>
+    </div>
+  );
+};
