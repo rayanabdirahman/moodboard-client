707b5fbd7819bc6a4ceb6977110354b9738db18d
rayanabdirahman <rayanmabdirahman@gmail.com>
2022-06-28 20:09:42 +0100

    reduce input group button size

--- src/components/screens/Home/index.tsx
+++ src/components/screens/Home/index.tsx
@@ -7,7 +7,6 @@ import {
   InputGroup,
   Button,
   InputRightElement,
-  Box,
 } from "@chakra-ui/react";
 import React from "react";
 
@@ -30,32 +29,34 @@ const HomeScreen: React.FC<Props> = () => {
           A place to shop and discover brands with friends
         </Heading>
         <Text fontSize="18px" color="gray.400">
-          Shop your favourite stores in one place
+          Shop your favourite stores in one place. Get notified when we launch.
         </Text>
       </Stack>
 
-      <InputGroup size="md" width="600px" mt="40px">
-        <Input
-          pr="4.5rem"
-          placeholder="Enter your email address"
-          size="lg"
-          bg="white"
-        />
-
-        <InputRightElement width="auto">
-          <Button
-            onClick={() => console.log("button clicked")}
+      <Stack textAlign="center" spacing="2">
+        <InputGroup size="md" width="600px" mt="40px">
+          <Input
+            pr="4.5rem"
+            placeholder="Enter your email address"
             size="lg"
-            mt="2"
-            bg="brand.500"
-            color="white"
-            borderTopLeftRadius="0"
-            borderBottomLeftRadius="0"
-          >
-            Get early access
-          </Button>
-        </InputRightElement>
-      </InputGroup>
+            bg="white"
+          />
+          <InputRightElement width="auto" mr="2">
+            <Button
+              onClick={() => console.log("button clicked")}
+              size="md"
+              mt="2"
+              bg="brand.500"
+              color="white"
+            >
+              Get early access
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+        <Text fontSize="16px" color="gray.400">
+          Notify me when you launch
+        </Text>
+      </Stack>
     </Flex>
   );
 };
